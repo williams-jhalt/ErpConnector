74fd823a918a64b1ec2a9f573c14d9291c43ffc2
John Halterman <johnh@williams-trading.com>
2020-03-13 10:42:27 -0400

    added pdf export

--- src/Controller/DefaultController.php
+++ src/Controller/DefaultController.php
@@ -4,6 +4,7 @@ namespace App\Controller;
 
 use App\Service\ErpOneConnector;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 
 class DefaultController extends AbstractController {
@@ -18,11 +19,14 @@ class DefaultController extends AbstractController {
     /**
      * Returns BASE64 encoded PDF document
      */
-    public function pdf($company, $type, $record, $sequence, ErpOneConnector $service) {
+    public function pdf($company, $type, Request $request, ErpOneConnector $service) {
+        
+        $record = $request->get('record');
+        $sequence = (int) $request->get('sequence');
         
         $company = filter_var($company, FILTER_SANITIZE_STRING);
         
-        $response = $service->getPdf($company, $type, $record, (int) $sequence);
+        $response = $service->getPdf($company, $type, $record, $sequence);
         
         return new Response($response->document);
         
